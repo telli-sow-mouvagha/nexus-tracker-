761e2ea7dad7d5cd9fe2b5459621bb911ea6bc03
Telli Sow-Mouvagha <tellisowmouvagha@gmail.com>
2026-02-04 21:43:36 +0100

    Intégration d'un bouton de suppression à chaque ticket

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const myContainer = document.querySelector("#ticket-container"); // query (les d
 // Gestion du clic
 myButton.addEventListener('click', ()=> {
     let ticket = document.createElement("div"); // id aléatoire
-    ticket.innerHTML ="je suis une div";
+    ticket.innerHTML ="Ticket (div)";
     ticket.setAttribute("id", "TK-" + Math.floor(Math.random()*999)); // prévoir vérif que ID = unique
     console.log(ticket.id); // debug id
+
+    let delBtn = document.createElement("button"); // bouton supprimer
+    delBtn.innerHTML = "x";
+
+    delBtn.addEventListener('click', ()=> {
+        myContainer.removeChild(ticket);
+    })
+
+    ticket.appendChild(delBtn); // à chaque ticket
     myContainer.appendChild(ticket);  
-    console.log("CLICK1"); // debug bouton
+    
+    console.log("CLICK"); // debug bouton
     console.log(myContainer.hasChildNodes()); // debug création div
 });
