89f2a2deb84a738f7f833956270d4b2860a136d4
Telli Sow-Mouvagha <tellisowmouvagha@gmail.com>
2026-02-05 11:31:58 +0100

    état instable : on supprime via un click sr le ticket pas sur le bouton

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ myButton.addEventListener('click', ()=> {
     let ticket = document.createElement("div"); // id aléatoire
     ticket.textContent ="Ticket (div)";
     ticket.setAttribute("id", "TK-" + Math.floor(Math.random()*999)); // prévoir vérif que ID = unique
-    console.log(ticket.id); // debug id
+    console.log("Ticket créé :", ticket.id); // debug id
 
     let delBtn = document.createElement("button"); // bouton supprimer
     delBtn.textContent = "x";
@@ -18,16 +18,19 @@ myButton.addEventListener('click', ()=> {
     ticket.appendChild(delBtn); // à chaque ticket
     myContainer.appendChild(ticket);  
     
-    console.log("CLICK"); // debug bouton
-    console.log(myContainer.hasChildNodes()); // debug création div
+    console.log("CLICK : Ajout"); // debug bouton
+    console.log("Le ticket est créé?: ",myContainer.hasChildNodes()); // debug création div
 });
 
 // Noeud parent fera hériter son EventListener à ses enfants
 myContainer.addEventListener('click', (event)=> {
+    // debug : click, event.target, child
+    console.log("CLICK : Suppression");
+    // if (event.target)... on vérifie (https://fr.javascript.info/event-delegation creuser ici)
     let ticket = event.target;
-    console.log(ticket); // debug event
+    console.log("Element cliqué :\n", ticket); // debug event
     // accès au premier enfant
-    console.log(ticket.children[0]);
+    console.log("Enfant de l'élément ticket :", ticket.children[0]);
     myContainer.removeChild(ticket);
     // event.target = <div></> (ticket)
 });
